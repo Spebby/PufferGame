3ba729e4a763036267f13c60dc57fb4496cb4abb
SettPaingHtin <135768054+SettPaingHtin@users.noreply.github.com>
2025-10-22 02:23:52 -0700

    Added BGM and Sound FX
    * Added BGM and Sound FX
    
    ---------
    
    Co-authored-by: Sett Paing Htin <shtin@ucsc.edu>
    Co-authored-by: Rxlling-Pxly <44181024+Rxlling-Pxly@users.noreply.github.com>

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -12,10 +12,18 @@ public class PlayerMovementController : MonoBehaviour {
     [SerializeField] float inAirGravity = 1f;
 
     public bool InWater { get; private set; }
-    WaterPool _waterPool; // :(
+    WaterPool _waterPool;
+
+    AudioSource _audioSource; 
+    
+    [Header("Audio")]
+    [SerializeField] AudioClip splashSound;
+    [SerializeField] AudioClip movingInWaterSound;
+    [SerializeField] AudioSource movementLoopSource; 
 
     void Awake() {
         _rigidBody2D = GetComponent<Rigidbody2D>();
+        _audioSource = GetComponent<AudioSource>(); 
     }
 
     void OnEnable() {
@@ -35,17 +43,36 @@ public class PlayerMovementController : MonoBehaviour {
     }
 
     void FixedUpdate() {
+        
+        bool isMovingInWater = InWater && _moveDirection2D.sqrMagnitude > 0.01f;
+        
+        if (isMovingInWater && !movementLoopSource.isPlaying) { 
+            movementLoopSource.clip = movingInWaterSound; 
+            movementLoopSource.loop = true; 
+            movementLoopSource.Play(); 
+        } 
+        else if (!isMovingInWater && movementLoopSource.isPlaying) { 
+            movementLoopSource.Stop(); 
+            movementLoopSource.loop = false; 
+        }
+        
         if (!_canMove) return;
         _rigidBody2D.AddForce(_moveDirection2D * moveForce);
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
-        // fyi layer is better than comparing to tag.
         if (!collision.gameObject.CompareTag("Water Pool")) return;
+        
+        bool wasInAir = !InWater; 
+
         _rigidBody2D.gravityScale = 0f;
         _canMove                  = true;
         InWater                   = true;
         _waterPool                = collision.gameObject.GetComponentInParent<WaterPool>();
+
+        if (wasInAir) {
+            _audioSource.PlayOneShot(splashSound); 
+        }
     }
 
     void OnTriggerExit2D(Collider2D collision) {
@@ -54,6 +81,11 @@ public class PlayerMovementController : MonoBehaviour {
         _canMove                  = false;
         InWater                   = false;
         _waterPool                = null;
+        
+        if (movementLoopSource.isPlaying) { 
+            movementLoopSource.Stop(); 
+            movementLoopSource.loop = false; 
+        }
     }
 
     public WaterPool GetWaterPool() {
--- Assets/Scripts/SpitAndDrink.cs
+++ Assets/Scripts/SpitAndDrink.cs
@@ -5,10 +5,11 @@ using UnityEngine.UI;
 
 
 public class SpitAndDrink : MonoBehaviour {
-	PlayerMovementController _player;
-	PlayerScale _scale;
-	Rigidbody2D _rigidBody2D;
-	
+    PlayerMovementController _player;
+    PlayerScale _scale;
+    Rigidbody2D _rigidBody2D;
+    AudioSource _audioSource; 
+
     [SerializeField] InputActionReference spitAction;
     [SerializeField] InputActionReference drinkAction;
 
@@ -18,113 +19,124 @@ public class SpitAndDrink : MonoBehaviour {
     [SerializeField, Range(0f, 1f)] float startWaterRatio = 0.25f;
     
     [Header("Spitting Behaviour")]
-	[SerializeField] GameObject waterBlobPrefab;
-	[SerializeField] float spitForce = 10f;
-	[SerializeField] float propulsionForce = 5f;
-	[SerializeField] float spitCost = 0.1f;
+    [SerializeField] GameObject waterBlobPrefab;
+    [SerializeField] float spitForce = 10f;
+    [SerializeField] float propulsionForce = 5f;
+    [SerializeField] float spitCost = 0.1f;
 
-	[SerializeField, Range(0f, 1f)] float minSpitSize = 0.2f;
-	[SerializeField] float maxChargeTime;
+    [SerializeField, Range(0f, 1f)] float minSpitSize = 0.2f;
+    [SerializeField] float maxChargeTime;
 
-	bool _isCharging;
-	float _chargeTime;
+    bool _isCharging;
+    float _chargeTime;
+    
+    [Header("Drinking Behaviour")]
+    [SerializeField, Tooltip("Units per-second")] float waterGainRate = 5f;
+    bool _isDrinking;
 
-	
-	[Header("Drinking Behaviour")]
-	[SerializeField, Tooltip("Units per-second")] float waterGainRate = 5f;
-	bool _isDrinking;
+    [Header("UI")]
+    [SerializeField] Image chargeSlider;
+    
+    [Header("Audio")] 
+    [SerializeField] AudioClip smallSpitSound; 
+    [SerializeField] AudioClip bigSpitSound; 
+    [SerializeField, Range(0f, 1f)] float bigSpitThreshold = 0.9f; 
 
-	[Header("UI")]
-	[SerializeField] Image chargeSlider;
-	
-	void Awake() {
-		_player      = GetComponent<PlayerMovementController>();
-		_scale       = GetComponent<PlayerScale>();
-		_rigidBody2D = GetComponent<Rigidbody2D>();
-		chargeSlider.fillAmount = 0f;
-		_waterAmount = maxWater * startWaterRatio;
-		
-		// this is rigid but should work ok. the scale script is the main problem here.
-		_scale.ModifyScale(_waterAmount);
-	}
+    void Awake() {
+        _player      = GetComponent<PlayerMovementController>();
+        _scale       = GetComponent<PlayerScale>();
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+        _audioSource = GetComponent<AudioSource>(); 
+        
+        chargeSlider.fillAmount = 0f;
+        _waterAmount = maxWater * startWaterRatio;
+        
+        _scale.ModifyScale(_waterAmount);
+    }
 
-	void Update() {
-		if (!_isDrinking || _waterAmount >= maxWater) return;
-		
-		Drink();
-		if (_waterAmount > maxWater) {
-			_waterAmount = maxWater;
-		}
-	}
+    void Update() {
+        if (!_isDrinking || _waterAmount >= maxWater) return;
+        
+        Drink();
+        if (_waterAmount > maxWater) {
+            _waterAmount = maxWater;
+        }
+    }
 
-	void OnEnable() {
-		spitAction.action.Enable();
-		spitAction.action.performed += StartCharge;
-		spitAction.action.canceled  += ReleaseCharge;
+    void OnEnable() {
+        spitAction.action.Enable();
+        spitAction.action.performed += StartCharge;
+        spitAction.action.canceled  += ReleaseCharge;
 
-		drinkAction.action.Enable();
-		drinkAction.action.performed += ToggleDrink;
-		drinkAction.action.canceled  += ToggleDrink;
-	}
+        drinkAction.action.Enable();
+        drinkAction.action.performed += ToggleDrink;
+        drinkAction.action.canceled  += ToggleDrink;
+    }
 
-	void OnDisable() {
-		spitAction.action.Disable();
-		spitAction.action.performed -= StartCharge;
-		spitAction.action.canceled  -= ReleaseCharge;
-		
-		drinkAction.action.Disable();
-		drinkAction.action.performed -= ToggleDrink;
-		drinkAction.action.canceled  -= ToggleDrink;
-	}
+    void OnDisable() {
+        spitAction.action.Disable();
+        spitAction.action.performed -= StartCharge;
+        spitAction.action.canceled  -= ReleaseCharge;
+        
+        drinkAction.action.Disable();
+        drinkAction.action.performed -= ToggleDrink;
+        drinkAction.action.canceled  -= ToggleDrink;
+    }
 
-	void FixedUpdate() {
-		if (!_isCharging) return;
-		_chargeTime += Time.fixedDeltaTime;
-		chargeSlider.fillAmount = _chargeTime / maxChargeTime;
-	}
+    void FixedUpdate() {
+        if (!_isCharging) return;
+        _chargeTime += Time.fixedDeltaTime;
+        chargeSlider.fillAmount = _chargeTime / maxChargeTime;
+    }
 
-	void StartCharge(InputAction.CallbackContext context) {
-		_isCharging             = true;
-		_chargeTime             = 0f;
-		chargeSlider.fillAmount = 0f;
-	}
+    void StartCharge(InputAction.CallbackContext context) {
+        _isCharging             = true;
+        _chargeTime             = 0f;
+        chargeSlider.fillAmount = 0f;
+    }
 
-	void ReleaseCharge(InputAction.CallbackContext context) {
-		if (!_isCharging) return;
-		_isCharging = false;
-		chargeSlider.fillAmount = 0;
-		float finalCharge = Mathf.Clamp(_chargeTime, maxChargeTime * minSpitSize, maxChargeTime) / maxChargeTime;
-		Spit(finalCharge);
-	}
-	
-	void Spit(float charge) {
-		float cost = charge * maxWater * spitCost;
-		if (_waterAmount < cost) return;
-		
-		Vector2 aim = (Camera.main!.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - transform.position).normalized;
-		GameObject blob = Instantiate(waterBlobPrefab, transform.position + (Vector3)aim * gameObject.transform.localScale.x * 1f, Quaternion.identity);
-		
-		blob.GetComponent<WaterBlob>().Initialise(cost);
-		blob.GetComponent<Rigidbody2D>().AddForce(aim * spitForce * charge, ForceMode2D.Impulse);
+    void ReleaseCharge(InputAction.CallbackContext context) {
+        if (!_isCharging) return;
+        _isCharging = false;
+        chargeSlider.fillAmount = 0;
+        float finalCharge = Mathf.Clamp(_chargeTime, maxChargeTime * minSpitSize, maxChargeTime) / maxChargeTime;
+        Spit(finalCharge);
+    }
+    
+    void Spit(float charge) {
+        float cost = charge * maxWater * spitCost;
+        if (_waterAmount < cost) return;
 
-		_waterAmount -= cost;
-		_rigidBody2D.AddForce(-aim * propulsionForce * charge, ForceMode2D.Impulse);
-		_scale.ModifyScale(-cost);
-	}
+        if (charge >= bigSpitThreshold) {
+            _audioSource.PlayOneShot(bigSpitSound); 
+        }
+        else {
+            _audioSource.PlayOneShot(smallSpitSound); 
+        }
+        
+        Vector2 aim = (Camera.main!.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - transform.position).normalized;
+        GameObject blob = Instantiate(waterBlobPrefab, transform.position + (Vector3)aim * gameObject.transform.localScale.x * 1f, Quaternion.identity);
+        
+        blob.GetComponent<WaterBlob>().Initialise(cost);
+        blob.GetComponent<Rigidbody2D>().AddForce(aim * spitForce * charge, ForceMode2D.Impulse);
 
-	void ToggleDrink(InputAction.CallbackContext _) {
-		_isDrinking = !_isDrinking;
-	}
+        _waterAmount -= cost;
+        _rigidBody2D.AddForce(-aim * propulsionForce * charge, ForceMode2D.Impulse);
+        _scale.ModifyScale(-cost);
+    }
 
-	void Drink() {
-		float cap = maxWater - _waterAmount;
-		if (cap < Mathf.Epsilon) return;
-		
-		WaterPool pool = _player.GetWaterPool();
-		// do a check on if player can actually drink more water
-		if (!pool || !pool.HasWater()) return;
-		float amount = pool.ReduceVolume(Mathf.Min(waterGainRate * Time.deltaTime, cap));
-		_waterAmount += amount;
-		_scale.ModifyScale(amount);
-	}
+    void ToggleDrink(InputAction.CallbackContext _) {
+        _isDrinking = !_isDrinking;
+    }
+
+    void Drink() {
+        float cap = maxWater - _waterAmount;
+        if (cap < Mathf.Epsilon) return;
+        
+        WaterPool pool = _player.GetWaterPool();
+        if (!pool || !pool.HasWater()) return;
+        float amount = pool.ReduceVolume(Mathf.Min(waterGainRate * Time.deltaTime, cap));
+        _waterAmount += amount;
+        _scale.ModifyScale(amount);
+    }
 }
